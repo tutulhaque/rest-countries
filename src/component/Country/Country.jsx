ca9ac70a2c93b238080b407a29febb5bb4b6a2b8
Md Saifuddullah <sdtutulctg@gmail.com>
2025-09-17 13:01:11 +0300

    props-validation-included-for-country

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useState } from "react";
 
 const Country = ({ country }) => {
   const { name, flags } = country;
   const [visited, setVisited] = useState(false);
+
   const handleVisited = () => {
     setVisited(!visited);
   };
+
   console.log(country);
+
   return (
     <div>
       <h3>{name?.common}</h3>
-      <img src={flags?.png} alt="" />
+      <img src={flags?.png} alt={name?.common || "country flag"} />
       <button onClick={handleVisited}>Visited country</button>
       {visited ? "Visited✅" : "Not Visited🚫"}
     </div>
   );
+};
+
+// prop validation
+Country.propTypes = {
+  country: PropTypes.shape({
+    name: PropTypes.shape({
+      common: PropTypes.string.isRequired,
+    }).isRequired,
+    flags: PropTypes.shape({
+      png: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default Country;
